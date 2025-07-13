51d443c774b702c72a109988231de11ae7626776
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-07-13 18:28:19 +0000

    [CONSTRUCCION DE TABLAS] model.py: Se escribio tabla de usuario y ordenes de servicio

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer, Enum
 from sqlalchemy.orm import Mapped, mapped_column
+import enum
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+# class User(db.Model):
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+#     password: Mapped[str] = mapped_column(nullable=False)
+#     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
+
+#Se definen los posibles estados de la orden de servicio con tipo de datos ENUM
+class RolEnum(enum.Enum):
+    INGRESADO = 'Ingresado'
+    EN_CURSO = 'En curso'
+    FINALIZADO = 'Finalizado'
+
+
+class Usuarios(db.Model):
+    __tablename__ = "usuarios"
+    id: Mapped[int] = mapped_column(primary_key = True)
+    nombre: Mapped[str] = mapped_column(String(80), nullable=False)
+    identificacion: Mapped[int] = mapped_column(Integer, unique=True, nullable = False)
+    password: Mapped[str] = mapped_column(String(12), nullable = False)
+    telefono: Mapped[str] = mapped_column(String(11))
+    email: Mapped[str] =  mapped_column(String(30), unique = True, Nullable = False)
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    foto_usuario: Mapped[str] = mapped_column(str(100))
+    rol: Mapped[RolEnum] = mapped_column(Enum(RolEnum), nullable=False)
+
+
+class Orden_de_trabajo(db.Model):
+    __tablename__ = "Orden_de_trabajo"
