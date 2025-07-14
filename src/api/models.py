fbc2b3b078f40b9ac0530a8851222e50fc74cbd3
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-07-14 04:06:31 +0000

    [SE INCLUYERON TODAS LAS LLAVES FORANEAS]

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, Enum, Date, Numeric
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, Enum, Date, Numeric, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 import enum, datetime
-
+from typing import List
 
 db = SQLAlchemy()
 
@@ -21,14 +21,21 @@ db = SQLAlchemy()
 #         }
 
 #Se definen los posibles estados de la orden de servicio con tipo de datos ENUM
+
+class AuxOrdenServicio(db.Model):
+    __tablename__ = "auxOrdenServicio"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    orden_id: Mapped[int] = mapped_column(ForeignKey("orden_de_trabajo.id_ot"), nullable = False)
+    servicio_id: Mapped[int] = mapped_column(ForeignKey("servicio.id_service"), nullable = False)
+
+
 class RolEnum(enum.Enum):
     MECANICO = 'Mecanico'
     CLIENTE = 'Cliente'
     
 
-
-class Usuarios(db.Model):
-    __tablename__ = "usuarios"
+class User(db.Model):
+    __tablename__ = "user"
     id_user: Mapped[int] = mapped_column(primary_key = True)
     nombre: Mapped[str] = mapped_column(String(80), nullable=False)
     identificacion: Mapped[int] = mapped_column(Integer, unique=True, nullable = False)
@@ -36,10 +43,10 @@ class Usuarios(db.Model):
     telefono: Mapped[str] = mapped_column(String(11))
     email: Mapped[str] =  mapped_column(String(30), unique = True, nullable = False)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    foto_usuario: Mapped[str] = mapped_column(str(100))
-    rol: Mapped[RolEnum] = mapped_column(Enum(RolEnum), nullable=False)
+    foto_usuario: Mapped[str] = mapped_column(String(100))
+    rol: Mapped[RolEnum] = mapped_column(Enum(RolEnum, name="rol_enum"), nullable=False)
 
-
+    
 class status(enum.Enum):
     INGRESADO = 'Ingresado'
     EN_PROCESO = 'En proceso'
@@ -51,8 +58,12 @@ class Orden_de_trabajo(db.Model):
     id_ot: Mapped[int] = mapped_column(primary_key = True)
     nombre_cliente: Mapped[str] = mapped_column(String(80), nullable=False)
     fecha_ingreso: Mapped[datetime.date] = mapped_column(Date, nullable=False)
-    estado_servicio: Mapped[status] = mapped_column(Enum(status), nullable = False)
+    estado_servicio: Mapped[status] = mapped_column(Enum(status, name="estado_orden"), nullable = False)
     fecha_final: Mapped[datetime.date] = mapped_column(Date, nullable = False)
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id_user"), nullable=False)
+    vehiculo_id: Mapped[int] = mapped_column(ForeignKey("vehiculos.id_vehiculo"), nullable = False)
+    mecanico_id: Mapped[int] = mapped_column(ForeignKey("user.id_user"), nullable = False)
 
 
 class Vehiculos(db.Model):
@@ -63,6 +74,8 @@ class Vehiculos(db.Model):
     modelo: Mapped[str] = mapped_column(String(15), nullable = False)
     year: Mapped[int] = mapped_column(Integer, nullable = False)
 
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id_user"), nullable = False)
+  
 class Servicio(db.Model):
     __tablename__ = 'servicio'
     id_service: Mapped[int] = mapped_column(primary_key = True)
